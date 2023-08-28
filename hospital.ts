80539885f8ac355676781c5e156bb3ea172fcee8
isrolik <isralerosn@gmail.com>
2023-08-28 12:08:54 +0300

    hosptle

--- hospital.ts
+++ hospital.ts
@@ -1,26 +1,51 @@
-class Person {
+abstract class Person {
   firstName: string;
   lastName: string;
-
-  constructor(firstName: string, lastName: string) {
+  age: number;
+  address: string;
+  constructor(
+    firstName: string,
+    lastName: string,
+    age: number,
+    address: string
+  ) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.age = age;
+    this.address = address;
   }
 }
 // 2:
 class Patient extends Person {
   patientID: number;
+  phoneNumber: number;
+  emergencyContact: number;
+  medicalHistory: Appointment[] = [];
 
-  constructor(firstName: string, lastName: string, patientID: number) {
-    super(firstName, lastName);
+  constructor(
+    firstName: string,
+    lastName: string,
+    age: number,
+    address: string,
+    patientID: number,
+    phoneNumber: number,
+    emergencyContact: number,
+    medicalHistory: Appointment[]
+  ) {
+    super(firstName, lastName, age, address);
     this.patientID = patientID;
+    this.phoneNumber = phoneNumber;
+    this.emergencyContact = emergencyContact;
+    this.medicalHistory = medicalHistory;
   }
 
   displayDetails() {
     console.log(
       `patient ID: ${this.patientID} name: ${this.firstName}, ${this.lastName}`
     );
+    console.log(`Medical History: ${this.medicalHistory.join(', ')}`);
   }
+
 }
 // 3:
 class Doctor extends Person {
@@ -30,10 +55,12 @@ class Doctor extends Person {
   constructor(
     firstName: string,
     lastName: string,
+    age: number,
+    address: string,
     doctorID: number,
     specialization: string
   ) {
-    super(firstName, lastName);
+    super(firstName, lastName, age, address);
     this.doctorID = doctorID;
     this.specialization = specialization;
   }
@@ -48,14 +75,14 @@ class Doctor extends Person {
 class Appointment {
   patient: Patient;
   doctor: Doctor;
-  date: string;
+  date: Date;
   time: string;
 
-  constructor(patient: Patient, doctor: Doctor, date: string, time: string) {
+  constructor(patient: Patient, doctor: Doctor, time: string) {
     this.patient = patient;
     this.doctor = doctor;
-    this.date = date;
     this.time = time;
+    this.date = new Date();
   }
   // 5:
   appointmentDetails() {
@@ -69,7 +96,7 @@ class Appointment {
 
 class Hospital {
   addPatients: Patient[] = [];
-  addDoctors: Doctor [] = [];
+  addDoctors: Doctor[] = [];
   appointments: Appointment[] = [];
   hospitalName: string;
 
@@ -83,14 +110,13 @@ class Hospital {
     this.addDoctors.push(doctor);
   }
   addAppointment(appointment: Appointment) {
-    
     this.appointments.push(appointment);
-    console.log("Appointments array",this.appointments)
+    console.log("Appointments array", this.appointments);
   }
   displayAllAppointments() {
     console.log("All Appointments:");
     this.appointments.forEach((appointment) => {
-      appointment.appointmentDetails()
+      appointment.appointmentDetails();
     });
   }
   displayDoctorAppointments(doctorID: number) {
@@ -110,21 +136,20 @@ class Hospital {
     });
   }
   displayTodayAppointments() {
-    const today = new Date()
+    const today = new Date();
     console.log(`Appointments for Today (${today}):`);
     this.appointments.forEach((appointment) => {
-        console.log(appointment)
-      if (appointment.date.toLocaleDateString() === today) {
+      console.log(appointment);
+      if (
+        appointment.date.toLocaleDateString() === today.toLocaleDateString()
+      ) {
         console.log(appointment.appointmentDetails());
       }
     });
   }
 }
-const newHospital = new Hospital("Har-Bracha")
-const newPatient = new Patient("Moshe", "Shalom", 1)
-const newDoctor = new Doctor("Rofe", "Ehad", 2, "Gastro")
-const newAppoitment = new Appointment(newPatient, newDoctor, "21-05-23", "10:40")
-newHospital.addAppointment(newAppoitment)
-
-
-console.log("I think that it's should work now!");
+// const newHospital = new Hospital("Har-Bracha");
+// const newPatient = new Patient("Moshe", "Shalom", 1);
+// const newDoctor = new Doctor("Rofe", "Ehad", 2, "Gastro");
+// const newAppoitment = new Appointment(newPatient, newDoctor, "10:40");
+// newHospital.addAppointment(newAppoitment);
